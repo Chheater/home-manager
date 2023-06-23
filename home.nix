ecbd412252e4ffb455e4ce4927b5013106cff458
Chheater <Chheater303@gmail.com>
2023-06-23 18:58:03 -0400

    add cosign

--- home.nix
+++ home.nix
@@ -33,7 +33,8 @@
 	pkgs.terminus-nerdfont
 	pkgs.acpi
 	pkgs.pandoc
-	pkgs.topgrade 
+	pkgs.topgrade
+	pkgs.cosign 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
